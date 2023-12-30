9029feb9d6efe3e8c9e7632495b5cb4dfd2402fd
ericw <ericweng92@gmail.com>
2023-12-31 03:15:38 +0800

    6

---
+++ app/src/main/java/com/example/chatapp/adapters/UsersAdapter.java
@@ -0,0 +1,57 @@
+package com.example.chatapp.adapters;
+
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.databinding.ItemContainerUserBinding;
+import com.example.chatapp.models.User;
+
+import java.util.List;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder>{
+
+    private final List<User> users;
+
+    public UsersAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemContainerUserBinding itemContainerUserBinding = ItemContainerUserBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new UserViewHolder(itemContainerUserBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.setUserData(users.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+
+        ItemContainerUserBinding binding;
+        UserViewHolder(ItemContainerUserBinding itemContainerUserBinding) {
+            super(itemContainerUserBinding.getRoot());
+            binding = itemContainerUserBinding;
+        }
+
+        void setUserData(User user){
+            binding.textName.setText(user.name);
+            binding.textNID.setText(user.NID);
+        }
+    }
+}
